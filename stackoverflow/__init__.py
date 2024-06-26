3f09e710c46657cb3548504e66b6a954db1ec899
Harsh Shastri <harshshastri@wsom1120.healthrite.lan>
2024-06-26 11:58:30 -0400

    Home page

--- stackoverflow/__init__.py
+++ stackoverflow/__init__.py
@@ -43,7 +43,7 @@ def search_questions(keyword, tag):
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Flask!"})
+    return jsonify({"message": "Welcome to Stack Over Flow App!"})
 
 
 @app.route('/search', methods=['GET'])
